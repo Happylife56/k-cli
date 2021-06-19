25b2266b6fb2774ab0ac086c2f6c0b4709c4839a
liukai <mkh0706@dingtalk.com>
2021-06-19 15:15:57 +0800

    vite-vue3-element模板添加stylelint验证以及cli常见项目名称的一优化

---
+++ bin/index.md
@@ -0,0 +1,7 @@
+### 测试
+
+1. 创建名字 aa 的项目
+
+```sh
+  node ./bin/index.js create aa
+```
--- templates/vite-vue3-element/src/views/about/index.vue
+++ templates/vite-vue3-element/src/views/about/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
+  <el-table :data="tableData" style="width: 100%;">
     <el-table-column prop="date" label="日期" width="180" />
     <el-table-column prop="name" label="姓名" width="180" />
     <el-table-column prop="address" label="地址" />
